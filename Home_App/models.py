76afc0621acddfecb4ce634ea5c934f86f8916dc
shalom <shalommwesigwa@gmail.com>
2019-05-19 15:26:07 +0300

    Modified templates to display categories from database.

--- Home_App/models.py
+++ Home_App/models.py
@@ -3,10 +3,10 @@ from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
-	Cat_Name = models.CharField(max_length=200)
+	cat_name = models.CharField(max_length=200)
 
 	def Add_To_Cat(self):
 		self.save()
 	def __str__(self):
-		return self.Cat_Name
+		return self.cat_name
 	
--- Home_App/views.py
+++ Home_App/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 def cat_list(request):
-	return render(request, 'Home_App/cat_list.html',{})
+	cats = Category.objects.all()
+	return render(request, 'Home_App/cat_list.html',{'cats': cats})
 	
