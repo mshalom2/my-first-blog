7604135366170be4da1c6cad7002960e8726b96a
shalom <shalommwesigwa@gmail.com>
2019-05-19 15:56:32 +0300

    Making DB changes

--- Home_App/models.py
+++ Home_App/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
 	cat_name = models.CharField(max_length=200)
 
+
 	def Add_To_Cat(self):
 		self.save()
-		
+
 	def __str__(self):
 		return self.cat_name
 	
